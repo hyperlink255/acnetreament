b2c062d5f0e39667c956d4ad99e14ac654911ff2
vikram singh <146583704+hyperlink255@users.noreply.github.com>
2024-08-16 16:00:51 +0530

    added index.html

---
+++ index.js
@@ -0,0 +1,38 @@
+const parallax = document.querySelector(".hero-section");
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.7 + "px";
+  // DIV 1 background will move slower than other elements on scroll.
+})
+var swiper = new Swiper(".review-slider", {
+spaceBetween:20,
+centeredSlides:true,
+autoplay:{
+   delay:7500,
+   disableOnInteraction:false,
+},
+loop:true,
+breakpoints: {
+   0: {
+       slidesPerView:1,
+   },
+   640:{
+       slidesPerView:2,
+   },
+   768:{
+       slidesPerView:2,
+   },
+   1024:{
+       slidesPerView:3,
+   },
+},
+});
+
+window.addEventListener('load', () => {
+    AOS.init({
+        duration: 500,
+        easing: 'fade-up',
+        once: true,
+        mirror: false
+    })
+});
